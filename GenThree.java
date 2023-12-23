88a9ff246563f759d7c0f60afd6761ca4b7752d7
Anit lubarsky <anit.lubarsky@runi.ac.il>
2023-12-23 23:14:31 +0200

    test homework 3

--- GenThree.java
+++ GenThree.java
@@ -6,14 +6,12 @@ public class GenThree {
 	public static void main(String[] args) {
 		int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
-		int x = (int) ((b-a)*Math.random()+a);
-		int y = (int) ((b-a)*Math.random()+a);
-		int z = (int) ((b-a)*Math.random()+a);
-		System.out.println(x);
-		System.out.println(y);
-		System.out.println(z);
-		int v =  Math.min(  x,  y);
-		int k =  Math.min( v, z);
-		System.out.println("The minimal generated number was" + k);
+		int x = (int)((b-a)*Math.random()+a);
+		int y = (int)((b-a)*Math.random()+a);
+		int z = (int)((b-a)*Math.random()+a);
+		System.out.println(x +"\n"+ y + "\n" + z);
+		int v = Math.min( x, y);
+		int k = Math.min( v, z);
+		System.out.println("The minimal generated number was " + k);
 	}
 }
